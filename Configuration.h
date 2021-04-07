aa1bb052e753558d869e54699f5024214ab806eb
fastriker <fastriker@users.noreply.github.com>
2021-04-08 00:27:27 +0200

    add compile date to printer name

--- Configuration.h
+++ Configuration.h
@@ -73,7 +73,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Kai Kientopf, myEnder-3-Pro)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(fastriker)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3-Pro"
+#define CUSTOM_MACHINE_NAME "myEnder-3-Pro" __DATE__ " " __TIME__
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
