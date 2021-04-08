843a50177df53fe78be9818630812b6be153cedc
fastriker <fastriker@users.noreply.github.com>
2021-04-08 14:53:43 +0200

    change preheat ABS to PETG

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1503,9 +1503,9 @@
 #define PREHEAT_1_TEMP_BED     45
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    110
+#define PREHEAT_2_LABEL       "PETG"
+#define PREHEAT_2_TEMP_HOTEND 210
+#define PREHEAT_2_TEMP_BED    70
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
 
 /**
